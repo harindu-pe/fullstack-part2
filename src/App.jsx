08cea8b09d43deeaad592f0ae38f593b765e9bc4
harindu-pe <harindu.pe@gmail.com>
2025-01-18 12:51:08 +0530

    exercise 2.5 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,4 @@
-const Header = ({ title }) => {
-  return <h1>{title}</h1>;
-};
-
-const Total = ({ sum }) => <b>total of {sum} exercises</b>;
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-
-const Content = ({ parts }) => (
-  <>
-    {parts.map((part, index) => (
-      <Part key={index} part={part} />
-    ))}
-  </>
-);
-
-const Course = ({ course }) => {
-  const partsArray = course.parts;
-
-  const addTwoNums = (a, b) => a + b.exercises;
-
-  const sum = partsArray.reduce(addTwoNums, 0);
-
-  return (
-    <>
-      <Header title={course.name} />
-      <Content parts={course.parts} />
-      <Total sum={sum} />
-    </>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
